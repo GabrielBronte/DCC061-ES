16a75d3155de8e1dcff21f33ae3accae9b896514
GabrielBronte <gabrielbronte@ice.ufjf.br>
2021-06-24 17:05:51 -0300

    Construindo inicio API

---
+++ API/Entidades/AppUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entidades
+{
+    public class AppUser
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string matricula { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+    }
+}
